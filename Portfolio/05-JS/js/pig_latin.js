05d94b22a7dc578f6a82b94e6944288a23e71ac5
CesarErnestoPerezGomez <0262141@up.edu.mx>
2024-10-26 01:29:53 -0600

    Update pig_latin.js

--- Portfolio/05-JS/js/pig_latin.js
+++ Portfolio/05-JS/js/pig_latin.js
@@ -1,33 +1,35 @@
-/*
-Pig Latin
-*/
-
 function igpayAtinlay(str) {
-  // TODO: Initialize the word array properly
-  var returnArray = [],
-    wordArray = [];
-  // TODO: make sure that the output is being properly built to produce the desired result.
+
+  var wordArray = str.split(" ");
+  var returnArray = [];
+
   for (var i = 0; i < wordArray.length; i++) {
-    var word = wordArray[i];
-    var beginning = word.charAt(0);
-
-    if (/[aeiouAEIOU]/.test(beginning)) {
-      returnArray.push(word);
-      continue;
-    }
-
-    for (var ii = 1; ii < word.length; ii++) {
-      if (/[aeiouAEIOU]/.test(word.charAt(ii))) {
-        break;
-      } else {
-        beginning += word.charAt(ii);
+      var word = wordArray[i];
+      var beginning = "";
+      
+      if (/[aeiouAEIOU]/.test(word.charAt(0))) {
+          returnArray.push(word + "way");
+          continue;
       }
-    }
+
+      for (var ii = 0; ii < word.length; ii++) {
+          if (/[aeiouAEIOU]/.test(word.charAt(ii))) {
+              returnArray.push(word.slice(ii) + beginning + "ay");
+              break;
+          } else {
+              beginning += word.charAt(ii);
+          }
+      }
   }
+
   return returnArray.join(" ");
 }
 
-// Some examples of expected outputs
-console.log(igpayAtinlay("pizza")); // "izzapay"
-console.log(igpayAtinlay("apple")); // "appleway"
-console.log(igpayAtinlay("happy meal")); // "appyhay ealmay"
+function displayPigLatin() {
+  const textInput = document.getElementById("textInput").value;
+  const resultElement = document.getElementById("result");
+
+  const pigLatinText = igpayAtinlay(textInput);
+  
+  resultElement.textContent = pigLatinText;
+}
