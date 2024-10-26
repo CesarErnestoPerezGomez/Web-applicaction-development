02560aab8776839e4ef457397bf78d058db1ddc0
CesarErnestoPerezGomez <0262141@up.edu.mx>
2024-10-26 01:43:11 -0600

    Update clock.js

--- Portfolio/05-JS/js/clock.js
+++ Portfolio/05-JS/js/clock.js
@@ -1,63 +1,76 @@
+var canvas = document.getElementById("clockCanvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.width / 2;
+ctx.translate(radius, radius); 
+radius = radius * 0.90; 
+
+function startClock() {
+    setInterval(drawClock, 1000); 
+}
+
 function drawClock() {
-  drawFace(ctx, radius);
-  drawNumbers(ctx, radius);
-  drawTime(ctx, radius);
+    drawFace(ctx, radius);
+    drawNumbers(ctx, radius);
+    drawTime(ctx, radius);
 }
 
 function drawFace(ctx, radius) {
-  var grad;
-  ctx.beginPath();
-  ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-  ctx.fillStyle = "white";
-  ctx.fill();
-
-  // Draw the edge circle with gradient
-  // TODO: (Optional) add a gradient circle
-
-  // Center circle
-  // TODO: make the central black circle
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI); 
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    ctx.strokeStyle = '#333';
+    ctx.lineWidth = 4;
+    ctx.stroke();
+    
+    ctx.beginPath();
+    ctx.arc(0, 0, 5, 0, 2 * Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();
 }
 
 function drawNumbers(ctx, radius) {
-  //TODO: Make sure you show all the numbers
-  var ang;
-  var num = 1;
-  ctx.font = radius * 0.15 + "px arial";
-  ctx.textBaseline = "middle";
-  ctx.fillStyle = "#333";
-  ctx.textAlign = "center";
-  ang = (num * Math.PI) / 6;
-  ctx.rotate(ang);
-  ctx.translate(0, -radius * 0.85);
-  ctx.rotate(-ang);
-  ctx.fillText(num.toString(), 0, 0);
-  ctx.rotate(ang);
-  ctx.translate(0, radius * 0.85);
-  ctx.rotate(-ang);
+    ctx.font = radius * 0.15 + "px Arial";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    for (var num = 1; num <= 12; num++) {
+        var ang = num * Math.PI / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-ang);
+    }
 }
 
 function drawTime(ctx, radius) {
-  // TODO: Calculate the angles of every hand depending on the time
-  var now = new Date();
-  var hour = now.getHours();
-  var minute = now.getMinutes();
-  var second = now.getSeconds();
-  //hour
-  hour = hour % 12;
-  drawHand(ctx, hour, radius * 0.5, radius * 0.07);
-  //minute
-  drawHand(ctx, minute, radius * 0.8, radius * 0.07);
-  // second
-  drawHand(ctx, second, radius * 0.9, radius * 0.02);
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+
+    hour = hour % 12;
+    hour = (hour * Math.PI / 6) + (minute * Math.PI / (6 * 60)) + (second * Math.PI / (360 * 60));
+    drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+
+    minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+    drawHand(ctx, minute, radius * 0.8, radius * 0.05);
+
+    second = (second * Math.PI / 30);
+    drawHand(ctx, second, radius * 0.9, radius * 0.02, "red");
 }
 
-function drawHand(ctx, pos, length, width) {
-  ctx.beginPath();
-  ctx.lineWidth = width;
-  ctx.lineCap = "round";
-  ctx.moveTo(0, 0);
-  ctx.rotate(pos);
-  ctx.lineTo(0, -length);
-  ctx.stroke();
-  ctx.rotate(-pos);
+function drawHand(ctx, pos, length, width, color = "#333") {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = color;
+    ctx.moveTo(0, 0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
 }
+
