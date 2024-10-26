8239fd791016ffb8998ac484d3168070c6923102
CesarErnestoPerezGomez <0262141@up.edu.mx>
2024-10-26 02:04:35 -0600

    Add files via upload

---
+++ Portfolio/07-BasicNode/Javascript node js/index.js
@@ -0,0 +1,9 @@
+const sh = require("superheroes");
+const sv = require("supervillains");
+
+const mySHname = sh.random();
+const mySVname = sv.random();
+
+console.log(`- Look it is ${mySVname} ready to do its evil deeds!`);
+console.log(`- Who could save us!`);
+console.log(`- Fear not! Here is ${mySHname} to the rescue!`);
