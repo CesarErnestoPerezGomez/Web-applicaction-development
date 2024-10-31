e7a8c0fd5c5ce8eaf13a842c53c6ff1a7a5abc56
CesarErnestoPerezGomez <0262141@up.edu.mx>
2024-10-30 19:46:49 -0600

    Update server.js

--- Portfolio/12-ExpressMongoose/server.js
+++ Portfolio/12-ExpressMongoose/server.js
@@ -1,6 +1,8 @@
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -8,10 +10,13 @@ app.use(express.static("public"));
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
 
-const mongoUrl = "mongodb://127.0.0.1:27017/f1";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Definition of a schema
+const mongoUrl = "mongodb://127.0.0.1:27017/f1";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+
 const teamSchema = new mongoose.Schema({
   id: Number,
   name: String,
@@ -35,6 +40,7 @@ driverSchema.set("strictQuery", true);
 const Team = mongoose.model("Team", teamSchema);
 const Driver = mongoose.model("Driver", driverSchema);
 
+
 let countries = [
   { code: "ENG", label: "England" },
   { code: "SPA", label: "Spain" },
@@ -53,10 +59,36 @@ let countries = [
   { code: "DEN", label: "Denmark" },
 ];
 
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/html/index.html");
 });
 
+app.get("/driver", async (req, res) => {
+  try {
+    const driver = new Driver({
+      num: req.query.num,
+      code: req.query.code,
+      forename: req.query.name,
+      surname: req.query.lname,
+      dob: req.query.dob,
+      nationality: req.query.nation,
+      url: req.query.url,
+      team: { name: req.query.team }, 
+    });
+
+    await driver.save();
+    res.send("Driver information saved successfully!");
+  } catch (err) {
+    console.error("Error saving driver information:", err);
+    res.status(500).send("Error saving driver information.");
+  }
+});
+
 app.listen(3000, (err) => {
-  console.log("Listening on port 3000");
+  if (err) {
+    console.error("Failed to start server:", err);
+  } else {
+    console.log("Listening on port 3000");
+  }
 });
